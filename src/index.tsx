32d2dde5c1d2c7634caa421fee8a3e29239f8e0b
Volodymyr Barybin <vohanchib@gmail.com>
2022-12-24 17:07:03 +0200

    injected service workers into react

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { App } from './app';
 
 
 render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
